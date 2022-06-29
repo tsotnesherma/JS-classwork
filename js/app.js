24691391c345164b7f4ed565846872065d386c8b
Tsotne Shermadini <tsotne.shermadini25b@geolab.edu.ge>
2022-06-29 16:17:49 +0400

    finished the classwork

---
+++ js/app.js
@@ -0,0 +1,8 @@
+let model = `Samsung Galaxy S22`;
+document.getElementById(`card-title`).innerHTML=model;
+
+let price= `500$`;
+document.getElementById(`card-price`).innerHTML=price;
+
+let text=`This is an awesome smartphone that can help you do a lot of thing in this modern era.`
+document.getElementById(`card-text`).innerHTML= text;
